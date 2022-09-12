5af9077e742dc84c1cc573bb9d97c683d27d0d07
Bruno Soares <tsoares.bruno@gmail.com>
2022-09-12 16:38:24 -0300

    Insere nome no glue job

--- infra/glue.tf
+++ infra/glue.tf
@@ -1,28 +1,4 @@
-# cria database
-# resource "aws_glue_catalog_database" "rais_db" {
-#   name = "rais"
-
-#   create_table_default_permission {
-#     permissions = ["SELECT"]
-
-#     principal {
-#       data_lake_principal_identifier = "IAM_ALLOWED_PRINCIPALS"
-#     }
-#   }
-# }
-
-# # cria crawler para a tabela rais
-# resource "aws_glue_crawler" "rais_crawler" {
-#   database_name = aws_glue_catalog_database.rais.name
-#   name          = "rais_crawler"
-#   role          = aws_iam_role.rais_crawler.arn
-
-#   s3_target {
-#     path = "s3://${aws_s3_bucket.rais_crawler.bucket}"
-#   }
-# }
-
-resource "aws_glue_job" "name" {
+resource "aws_glue_job" "job-rais-parquet" {
   name = "glue_job_rais"
   role_arn = ""
   description = "Job Glue para realizar pequenos ajustes na RAIS e salvar no formato parquet"
